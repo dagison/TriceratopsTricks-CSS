15ff2fdd0810e0e508d1e880119dd011ef7fae25
The Dagison <64185976+dagison@users.noreply.github.com>
2020-08-06 18:10:56 +0800

    additional captions for alertbox

--- how_to_center_anything_in_css/alert.js
+++ how_to_center_anything_in_css/alert.js
@@ -1,21 +1,21 @@
 function firstFunction() {
-	swal("container {\n display: grid;\n place-items: center;\n }");
+	swal("container {\n display: grid;\n place-items: center;\n }\n \n CSS Grid Layout excels at dividing a page into major regions or defining the relationship in terms of size, position, and layer, between parts of a control built from HTML primitives. Like tables, grid layout enables an author to align elements into columns and rows.");
 }
 function secondFunction() {
-	swal("container {\n display: block;\n margin: auto;\n }");
+	swal("container {\n display: block;\n margin: auto;\n }\n \n A block-level element always starts on a new line and takes up the full width available (stretches out to the left and right as far as it can). The <div> element is a block-level element. Examples of block-level elements: <div>");
 }
 function thirdFunction() {
-	swal("container {\n display: flex;\n justify-content: center;\n align-items: center;\n }");
+	swal("container {\n display: flex;\n justify-content: center;\n align-items: center;\n }\n \n A flex container expands items to fill available free space or shrinks them to prevent overflow. Most importantly, the flexbox layout is direction-agnostic as opposed to the regular layouts (block which is vertically-based and inline which is horizontally-based).");
 }
 function fourthFunction() {
-	swal("container {\n text-align: center;\n }");
+	swal("container {\n text-align: center;\n }\n \n The text-align property is used to set the horizontal alignment of a text. ");
 }
 function insertedFunction() {
-	swal("container {\n width: 50%;\n margin: 0 25%;\n }");
+	swal("container {\n width: 50%;\n margin: 0 25%;\n }\n \n Here, we used the width to put the original 100% to 50% and set a margin value for horizontal axis, in order for the object to be centered.");
 }
 function fifthFunction() {
-	swal("container {\n position: absolute;\n top: 50%;\n left: 50%;\n transform: translate(-50%, -50%);\n }");
+	swal("container {\n position: absolute;\n top: 50%;\n left: 50%;\n transform: translate(-50%, -50%);\n }\n \n An element with position: absolute; is positioned relative to the nearest positioned ancestor (instead of positioned relative to the viewport, like fixed). However; if an absolute positioned element has no positioned ancestors, it uses the document body, and moves along with page scrolling.");
 }
 function doNotFunction() {
-	swal("<center> on HTML");
+	swal("<center> on HTML\n \n The <center> tag was used in HTML4 to center-align text which is nowadays considered as a bad practice because it is not supported anymore in HTML5.");
 }
