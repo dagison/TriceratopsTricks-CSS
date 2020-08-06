c71d8e73dff186e8940fa33b8c885128955450d2
The Dagison <64185976+dagison@users.noreply.github.com>
2020-08-06 12:16:04 +0800

    S1- How to Center Anything in CSS

---
+++ how_to_center_anything_in_css/alert.js
@@ -0,0 +1,21 @@
+function firstFunction() {
+	swal("container {\n display: grid;\n place-items: center;\n }");
+}
+function secondFunction() {
+	swal("container {\n display: block;\n margin: auto;\n }");
+}
+function thirdFunction() {
+	swal("container {\n display: flex;\n justify-content: center;\n align-items: center;\n }");
+}
+function fourthFunction() {
+	swal("container {\n text-align: center;\n }");
+}
+function insertedFunction() {
+	swal("container {\n width: 50%;\n margin: 0 25%;\n }");
+}
+function fifthFunction() {
+	swal("container {\n position: absolute;\n top: 50%;\n left: 50%;\n transform: translate(-50%, -50%);\n }");
+}
+function doNotFunction() {
+	swal("<center> on HTML");
+}
